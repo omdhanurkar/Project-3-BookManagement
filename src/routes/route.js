63818204572eb51f4298bf603b5c2a637ecc6cf5
Adesh Singh Rajawat <aadeshsingh1947@gmail.com>
2022-09-20 13:50:26 +0530

    user creation

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const userController= require('../controllers/userController')
 //----------dummy-------------
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+
+router.post("/register/users",userController.createUser)
 
 module.exports = router
